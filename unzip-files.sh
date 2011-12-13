503a6e2e5d1b8139658da58b56a2b8b80b6138fa
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2011-12-13 17:51:28 +0700

    update unzip-files

--- unzip-files.sh
+++ unzip-files.sh
@@ -29,93 +29,100 @@ mkdir -p ./vendor/$MANUFACTURER/$DEVICE/proprietary/wifi/ath6k
 mkdir -p ./vendor/$MANUFACTURER/$DEVICE/proprietary/wifi/ath6k/AR6003
 mkdir -p ./vendor/$MANUFACTURER/$DEVICE/proprietary/wifi/ath6k/AR6003/hw2.0
 
-
 # rill
-mv ./${DEVICE}_update/system/lib/libril.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libsecril-client.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libsec-ril.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libdiag.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libril.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libsecril-client.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libsec-ril.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libdiag.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/bin/qmuxd ./vendor/$MANUFACTURER/$DEVICE/proprietary/bin
+mv ./tass_update/system/bin/rild ./vendor/$MANUFACTURER/$DEVICE/proprietary/bin
 
 # omx libs
-mv ./${DEVICE}_update/system/lib/libmm-adspsvc.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxAacDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxAacEnc.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxAdpcmDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxAmrDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxAmrEnc.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxAmrRtpDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxAmrwbDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxEvrcEnc.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxEvrcHwDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxH264Dec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxMp3Dec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxMpeg4Dec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxOn2Dec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxQcelp13Enc.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxQcelpHwDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxVidEnc.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxWmaDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libOmxWmvDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libmm-adspsvc.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxAacDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxAacEnc.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxAdpcmDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxAmrDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxAmrEnc.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxAmrRtpDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxAmrwbDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxEvrcEnc.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxEvrcHwDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxH264Dec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxMp3Dec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxMpeg4Dec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxOn2Dec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxQcelp13Enc.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxQcelpHwDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxVidEnc.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxWmaDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libOmxWmvDec.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
 
 # omx shared libs
-mv ./${DEVICE}_update/system/lib/libomx_aacdec_sharedlibrary.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libomx_amrdec_sharedlibrary.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libomx_amrenc_sharedlibrary.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libomx_avcdec_sharedlibrary.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libomx_m4vdec_sharedlibrary.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libomx_mp3dec_sharedlibrary.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libomx_sharedlibrary.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libomx_aacdec_sharedlibrary.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libomx_amrdec_sharedlibrary.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libomx_amrenc_sharedlibrary.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libomx_avcdec_sharedlibrary.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libomx_m4vdec_sharedlibrary.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libomx_mp3dec_sharedlibrary.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libomx_sharedlibrary.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
 
 # camera
-mv ./${DEVICE}_update/system/lib/liboemcamera.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libmmjpeg.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libmmipl.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libs3cjpeg.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libActionShot.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libarccamera.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libCaMotion.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libcaps.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libPanoraMax1.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libPlusMe.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libseccamera.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libseccameraadaptor.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libcamera.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libcamera_client.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libcameraservice.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-
-# audio
-mv ./${DEVICE}_update/system/lib/liba2dp.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/libaudioeq.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-
-# gps
-mv ./${DEVICE}_update/system/lib/hw/gps.msm7k.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/hw
-mv ./${DEVICE}_update/system/bin/gpsd ./vendor/$MANUFACTURER/$DEVICE/proprietary/bin
-
-# lights
-mv ./${DEVICE}_update/system/lib/hw/lights.msm7k.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/hw
-
-# sensor
-mv ./${DEVICE}_update/system/lib/hw/sensors.default.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/hw
-mv ./${DEVICE}_update/system/bin/memsicd ./vendor/$MANUFACTURER/$DEVICE/proprietary/bin
-
-# camera
-mv ./${DEVICE}_update/system/lib/egl/libGLESv1_CM_adreno200.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/egl
-mv ./${DEVICE}_update/system/lib/egl/libq3dtools_adreno200.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/egl
-mv ./${DEVICE}_update/system/lib/egl/libEGL_adreno200.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/egl
-mv ./${DEVICE}_update/system/lib/egl/libGLES_android.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/egl
-mv ./${DEVICE}_update/system/lib/egl/libGLESv2_adreno200.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/egl
-mv ./${DEVICE}_update/system/etc/firmware/yamato_pfp.fw ./vendor/$MANUFACTURER/$DEVICE/proprietary/etc/firmware
-mv ./${DEVICE}_update/system/etc/firmware/yamato_pm4.fw ./vendor/$MANUFACTURER/$DEVICE/proprietary/etc/firmware
-mv ./${DEVICE}_update/system/lib/libgsl.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
-mv ./${DEVICE}_update/system/lib/egl/egl.cfg ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/egl
+mv ./tass_update/system/lib/liboemcamera.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libmmjpeg.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libmmipl.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libs3cjpeg.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libActionShot.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libarccamera.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libCaMotion.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libcaps.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libPanoraMax1.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libPlusMe.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libseccamera.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libseccameraadaptor.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libcamera.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libcamera_client.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libcameraservice.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
 
 # wifi
-mv ./${DEVICE}_update/system/wifi/ar6000.ko ./vendor/$MANUFACTURER/$DEVICE/proprietary/wifi
-mv ./${DEVICE}_update/system/wifi/ath6k/AR6003/hw2.0/athtcmd_ram.bin ./vendor/$MANUFACTURER/$DEVICE/proprietary/wifi/ath6k/AR6003/hw2.0
-mv ./${DEVICE}_update/system/wifi/ath6k/AR6003/hw2.0/athwlan.bin.z77 ./vendor/$MANUFACTURER/$DEVICE/proprietary/wifi/ath6k/AR6003/hw2.0
-mv ./${DEVICE}_update/system/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin ./vendor/$MANUFACTURER/$DEVICE/proprietary/wifi/ath6k/AR6003/hw2.0
-mv ./${DEVICE}_update/system/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin.01 ./vendor/$MANUFACTURER/$DEVICE/proprietary/wifi/ath6k/AR6003/hw2.0
-mv ./${DEVICE}_update/system/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin.02 ./vendor/$MANUFACTURER/$DEVICE/proprietary/wifi/ath6k/AR6003/hw2.0
-mv ./${DEVICE}_update/system/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin.03 ./vendor/$MANUFACTURER/$DEVICE/proprietary/wifi/ath6k/AR6003/hw2.0
-mv ./${DEVICE}_update/system/wifi/ath6k/AR6003/hw2.0/data.patch.bin ./vendor/$MANUFACTURER/$DEVICE/proprietary/wifi/ath6k/AR6003/hw2.0
-mv ./${DEVICE}_update/system/wifi/ath6k/AR6003/hw2.0/otp.bin.z77 ./vendor/$MANUFACTURER/$DEVICE/proprietary/wifi/ath6k/AR6003/hw2.0
+mv ./tass_update/system/wifi/ar6000.ko ./vendor/$MANUFACTURER/$DEVICE/proprietary/wifi
+mv ./tass_update/system/wifi/ath6k/AR6003/hw2.0/athtcmd_ram.bin ./vendor/$MANUFACTURER/$DEVICE/proprietary/ath6k/AR6003/hw2.0
+mv ./tass_update/system/wifi/ath6k/AR6003/hw2.0/athwlan.bin.z77 ./vendor/$MANUFACTURER/$DEVICE/proprietary/ath6k/AR6003/hw2.0
+mv ./tass_update/system/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin ./vendor/$MANUFACTURER/$DEVICE/proprietary/ath6k/AR6003/hw2.0
+mv ./tass_update/system/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin.01 ./vendor/$MANUFACTURER/$DEVICE/proprietary/ath6k/AR6003/hw2.0
+mv ./tass_update/system/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin.02 ./vendor/$MANUFACTURER/$DEVICE/proprietary/ath6k/AR6003/hw2.0
+mv ./tass_update/system/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin.03 ./vendor/$MANUFACTURER/$DEVICE/proprietary/ath6k/AR6003/hw2.0
+mv ./tass_update/system/wifi/ath6k/AR6003/hw2.0/data.patch.bin ./vendor/$MANUFACTURER/$DEVICE/proprietary/ath6k/AR6003/hw2.0
+mv ./tass_update/system/wifi/ath6k/AR6003/hw2.0/otp.bin.z77 ./vendor/$MANUFACTURER/$DEVICE/proprietary/ath6k/AR6003/hw2.0
+mv ./tass_update/system/bin/hostapd ./vendor/$MANUFACTURER/$DEVICE/proprietary/bin
+mv ./tass_update/system/bin/wlan_mac ./vendor/$MANUFACTURER/$DEVICE/proprietary/bin
+mv ./tass_update/system/bin/wlan_tool ./vendor/$MANUFACTURER/$DEVICE/proprietary/bin
+mv ./tass_update/system/bin/wmiconfig ./vendor/$MANUFACTURER/$DEVICE/proprietary/bin
+
+# bluetooth
+mv ./tass_update/system/bin/BCM2049C0_003.001.031.0088.0094.hcd ./vendor/$MANUFACTURER/$DEVICE/proprietary/bin
+
+# audio
+mv ./tass_update/system/lib/liba2dp.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/libaudioeq.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+
+# gps
+mv ./tass_update/system/lib/hw/gps.msm7k.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/hw
+mv ./tass_update/system/bin/gpsd ./vendor/$MANUFACTURER/$DEVICE/proprietary/bin
+
+# lights
+mv ./tass_update/system/lib/hw/lights.msm7k.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/hw
+
+# sensor
+mv ./tass_update/system/lib/hw/sensors.default.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/hw
+mv ./tass_update/system/bin/memsicd ./vendor/$MANUFACTURER/$DEVICE/proprietary/bin
+
+# camera
+mv ./tass_update/system/lib/egl/libGLESv1_CM_adreno200.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/egl
+mv ./tass_update/system/lib/egl/libq3dtools_adreno200.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/egl
+mv ./tass_update/system/lib/egl/libEGL_adreno200.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/egl
+mv ./tass_update/system/lib/egl/libGLESv2_adreno200.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/egl
+mv ./tass_update/system/etc/firmware/yamato_pfp.fw ./vendor/$MANUFACTURER/$DEVICE/proprietary/etc/firmware
+mv ./tass_update/system/etc/firmware/yamato_pm4.fw ./vendor/$MANUFACTURER/$DEVICE/proprietary/etc/firmware
+mv ./tass_update/system/lib/libgsl.so ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib
+mv ./tass_update/system/lib/egl/egl.cfg ./vendor/$MANUFACTURER/$DEVICE/proprietary/lib/egl
